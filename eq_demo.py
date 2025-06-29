67bef4659c47de4e48b629c1dcad0c5b413e029c
Benz Poobua <supakorn1207@gmail.com>
2025-06-29 15:35:27 +0700

    Add functionality

--- eq_demo.py
+++ eq_demo.py
@@ -33,3 +33,63 @@ print(f'Distance = {distance_km_obspy:.2f} km')
 print(f'Distance = {distance_deg:.2f} degrees')
 print(f'Azimuth (event to station) = {azimuth:.2f}°')
 print(f'Back-azimuth (station to event) = {back_azimuth:.2f}°')
+
+### Step 2: Visual the waveforms
+
+# Create velocity model
+model = TauPyModel(model = 'iasp91')
+
+# Calculate travel times
+arrivals = model.get_travel_times(
+    source_depth_in_km = hualien_event.depth.to_numpy()[0], 
+    distance_in_degree=distance_deg
+)
+
+# List all arrivals
+print('Available seismic phases and arrival times:')
+for arrival in arrivals:
+    print(f'Phase: {arrival.name}, Arrival Time: {arrival.time:.2f} sec')
+
+# Download seismic data
+network = 'IU'
+station = 'TATO'
+channel = 'BH*'
+starttime = UTCDateTime('2024-04-22T23:58:11')
+endtime = starttime + 60 * 15 # 15 minutes window
+
+client = Client('IRIS')
+
+# Get waveform data with instrument response
+st = client.get_waveforms(network=network, station=station, location='00', channel=channel, starttime=starttime, endtime=endtime, attach_response=True)
+
+# Remove instrument response (convert to velocity)
+st.remove_response(output='VEL')
+
+# Basic preprocessing
+st.merge(fill_value='interpolate')  # Merge traces if gaps
+st.detrend(type='linear')           # Remove linear trend
+st.taper(max_percentage=0.05)       # Apply taper
+
+print(st)
+
+# Plot the waveform
+st[2].plot() # Plot only Z component
+
+# Plot the spectrogram
+st[2].spectrogram(log=True)
+
+# Filter the waveform
+tr = st[2].copy()
+tr.filter('bandpass', freqmin=1.0, freqmax=5.0, corners=4, zerophase=True)
+tr.plot()
+
+# Save the seismogram for later use; `MSEED` format
+tr.write("hualien.mseed", format="MSEED")
+
+# Save the figure
+fig = tr.plot(show=False)
+fig.savefig('Waveform.png', dpi=300)
+
+
+# !pip install pyrocko
+# !snuffler hualien.mseed
